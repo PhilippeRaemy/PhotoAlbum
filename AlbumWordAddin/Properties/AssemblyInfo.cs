eb38c29759026a28d15d27186aad7f778dbcc423
philipperaemy <philippe_raemy@swissonline.ch>
2016-10-22 20:43:00 +0200

    Sign with test certificate

--- AlbumWordAddin/Properties/AssemblyInfo.cs
+++ AlbumWordAddin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AlbumWordAddin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A pictures management helper for photo albums in MSWORD")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Raemy Lalive and Kids")]
 [assembly: AssemblyProduct("AlbumWordAddin")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Philippe Raemy 2016")]
+[assembly: AssemblyTrademark("Raemy Lalive and Kids")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -30,7 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
 
