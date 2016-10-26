7bc46ae749063b2670b23aca59ebb481d7dee33b
philipperaemy <philippe_raemy@swissonline.ch>
2016-10-26 17:55:21 +0200

    TestPositioner_FourInaRowTopNoPadding & TestPositioner_FourInaColTopNoPadding

--- PositionerTests/PositionerTests.cs
+++ PositionerTests/PositionerTests.cs
@@ -29,14 +29,14 @@ namespace PositionerTests
         static readonly Rectangle R4X4 = new Rectangle(0, 0, 4, 4);
         static readonly Rectangle R4X2 = new Rectangle(0, 0, 4, 2);
         static readonly Rectangle R2X4 = new Rectangle(0, 0, 2, 4);
-        static readonly Positioner HFlatPos        = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0    };
-        static readonly Positioner HFlatPosPad     = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0.1f };
-        static readonly Positioner HFlatLeftPos    = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Left, VShape = VShape.Flat, Margin = 0, Padding = 0    };
-        static readonly Positioner HFlatLeftPosPad = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Left, VShape = VShape.Flat, Margin = 0, Padding = 0.1f };
-        static readonly Positioner VFlatPos        = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0    };
-        static readonly Positioner VFlatPosPad     = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0.1f };
-        static readonly Positioner VFlatLeftPos    = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Left, VShape = VShape.Flat, Margin = 0, Padding = 0    };
-        static readonly Positioner VFlatLeftPosPad = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Left, VShape = VShape.Flat, Margin = 0, Padding = 0.1f };
+        static readonly Positioner HFlatPos     = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0    };
+        static readonly Positioner HFlatPosPad  = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0.1f };
+        static readonly Positioner HFlatLeftPos = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Left, VShape = VShape.Flat, Margin = 0, Padding = 0    };
+        static readonly Positioner HFlatTopPos  = new Positioner { Cols = 4, Rows = 1, HShape = HShape.Flat, VShape = VShape.Top , Margin = 0, Padding = 0};
+        static readonly Positioner VFlatPos     = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0    };
+        static readonly Positioner VFlatPosPad  = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Padding = 0.1f };
+        static readonly Positioner VFlatLeftPos = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Left, VShape = VShape.Flat, Margin = 0, Padding = 0    };
+        static readonly Positioner VFlatTopPos  = new Positioner { Cols = 1, Rows = 4, HShape = HShape.Flat, VShape = VShape.Top , Margin = 0, Padding = 0};
 
         [TestMethod]
         public void TestPositioner_1x1()
@@ -108,6 +108,13 @@ namespace PositionerTests
             TestPositioner_global(R4X1, R2X4.Range(4), HFlatLeftPos, R2X4.Grow(.25f).Range(4, 1, 0), nameof(R2X4));
         }
         [TestMethod]
+        public void TestPositioner_FourInaRowTopNoPadding()
+        {
+            TestPositioner_global(R4X1, R1X1.Range(4), HFlatTopPos, R1X1.Range(4, 1, 0), nameof(R1X1));
+            TestPositioner_global(R4X1, R4X2.Range(4), HFlatTopPos, R4X2.Grow(.25f).Range(4, 1, 0), nameof(R4X2));
+            TestPositioner_global(R4X1, R2X4.Range(4), HFlatTopPos, R2X4.Move(.25f, 0).Grow(.25f).Range(4, 1, 0), nameof(R2X4));
+        }
+        [TestMethod]
         public void TestPositioner_FourInaColFlatNoPadding()
         {
             TestPositioner_global(R1X4, R1X1.Range(4), VFlatPos, R1X1.Range(4, 0, 1), nameof(R1X1));
@@ -125,6 +132,13 @@ namespace PositionerTests
             TestPositioner_global(R1X4, R1X1.Range(4), VFlatLeftPos, R1X1.Range(4, 0, 1), nameof(R1X1));
             TestPositioner_global(R1X4, R4X2.Range(4), VFlatLeftPos, R4X2.Move(0, .25f).Grow(.25f).Range(4, 0, 1), nameof(R4X2));
             TestPositioner_global(R1X4, R2X4.Range(4), VFlatLeftPos, R2X4.Grow(.25f).Range(4, 0, 1), nameof(R2X4));
+        }
+        [TestMethod]
+        public void TestPositioner_FourInaColTopNoPadding()
+        {
+            TestPositioner_global(R1X4, R1X1.Range(4), VFlatTopPos, R1X1.Range(4, 0, 1), nameof(R1X1));
+            TestPositioner_global(R1X4, R4X2.Range(4), VFlatTopPos, R4X2.Grow(.25f).Range(4, 0, 1), nameof(R4X2));
+            TestPositioner_global(R1X4, R2X4.Range(4), VFlatTopPos, R2X4.Move(.25f, 0).Grow(.25f).Range(4, 0, 1), nameof(R2X4));
         }
         static void TestPositioner_global(Rectangle clientArea, IEnumerable<Rectangle> rectangles, Positioner pos, IEnumerable<Rectangle> expected, string label)
         {
