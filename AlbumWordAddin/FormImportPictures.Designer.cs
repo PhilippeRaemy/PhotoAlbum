32543adcc591bb248bf333679a1d375fe47e90f7
philipperaemy <philippe_raemy@swissonline.ch>
2016-11-27 08:08:19 +0100

    Visual changes

--- AlbumWordAddin/FormImportPictures.Designer.cs
+++ AlbumWordAddin/FormImportPictures.Designer.cs
@@ -44,6 +44,8 @@
             this.panelProgress = new System.Windows.Forms.Panel();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.labelProgress = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.panelStartFolder.SuspendLayout();
             this.panelEndFolder.SuspendLayout();
             this.panelButtons.SuspendLayout();
@@ -132,20 +134,21 @@
             // 
             // panelButtons
             // 
+            this.panelButtons.Controls.Add(this.label4);
             this.panelButtons.Controls.Add(this.label3);
             this.panelButtons.Controls.Add(this.buttonGo);
             this.panelButtons.Controls.Add(this.comboMaxPicsPerFile);
             this.panelButtons.Controls.Add(this.ChkConfirmOverwrite);
             this.panelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelButtons.Location = new System.Drawing.Point(0, 50);
+            this.panelButtons.Location = new System.Drawing.Point(0, 71);
             this.panelButtons.Name = "panelButtons";
-            this.panelButtons.Size = new System.Drawing.Size(710, 39);
+            this.panelButtons.Size = new System.Drawing.Size(710, 57);
             this.panelButtons.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(155, 8);
+            this.label3.Location = new System.Drawing.Point(12, 27);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 13);
             this.label3.TabIndex = 10;
@@ -153,7 +156,7 @@
             // 
             // buttonGo
             // 
-            this.buttonGo.Location = new System.Drawing.Point(461, 3);
+            this.buttonGo.Location = new System.Drawing.Point(632, 28);
             this.buttonGo.Name = "buttonGo";
             this.buttonGo.Size = new System.Drawing.Size(75, 23);
             this.buttonGo.TabIndex = 9;
@@ -170,7 +173,7 @@
             "100",
             "150",
             "Unlimited"});
-            this.comboMaxPicsPerFile.Location = new System.Drawing.Point(265, 5);
+            this.comboMaxPicsPerFile.Location = new System.Drawing.Point(130, 24);
             this.comboMaxPicsPerFile.Name = "comboMaxPicsPerFile";
             this.comboMaxPicsPerFile.Size = new System.Drawing.Size(121, 21);
             this.comboMaxPicsPerFile.TabIndex = 8;
@@ -179,11 +182,10 @@
             // ChkConfirmOverwrite
             // 
             this.ChkConfirmOverwrite.AutoSize = true;
-            this.ChkConfirmOverwrite.Location = new System.Drawing.Point(16, 7);
+            this.ChkConfirmOverwrite.Location = new System.Drawing.Point(130, 8);
             this.ChkConfirmOverwrite.Name = "ChkConfirmOverwrite";
-            this.ChkConfirmOverwrite.Size = new System.Drawing.Size(133, 17);
+            this.ChkConfirmOverwrite.Size = new System.Drawing.Size(15, 14);
             this.ChkConfirmOverwrite.TabIndex = 7;
-            this.ChkConfirmOverwrite.Text = "Confirm File Overwrites";
             this.ChkConfirmOverwrite.UseVisualStyleBackColor = true;
             this.ChkConfirmOverwrite.CheckedChanged += new System.EventHandler(this.ChkConfirmOverwrite_CheckedChanged);
             // 
@@ -192,7 +194,7 @@
             this.panelProgress.Controls.Add(this.progressBar);
             this.panelProgress.Controls.Add(this.labelProgress);
             this.panelProgress.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelProgress.Location = new System.Drawing.Point(0, 89);
+            this.panelProgress.Location = new System.Drawing.Point(0, 128);
             this.panelProgress.Name = "panelProgress";
             this.panelProgress.Size = new System.Drawing.Size(710, 43);
             this.panelProgress.TabIndex = 10;
@@ -214,11 +216,24 @@
             this.labelProgress.Size = new System.Drawing.Size(0, 13);
             this.labelProgress.TabIndex = 10;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Confirm overwrite files:";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
             // FormImportPictures
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(710, 132);
+            this.ClientSize = new System.Drawing.Size(710, 171);
             this.Controls.Add(this.panelButtons);
             this.Controls.Add(this.panelProgress);
             this.Controls.Add(this.panelEndFolder);
@@ -255,5 +270,7 @@
         private System.Windows.Forms.Panel panelProgress;
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
