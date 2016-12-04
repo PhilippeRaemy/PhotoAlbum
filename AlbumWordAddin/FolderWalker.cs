9e3905c4e5cc2a906135e2f7c0d673551b71841b
PhilippeRaemy <philippe_raemy@swissonline.ch>
2016-12-04 19:59:46 +0100

    FolderWalker

---
+++ AlbumWordAddin/FolderWalker.cs
@@ -0,0 +1,117 @@
+﻿namespace AlbumWordAddin
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using MoreLinq;
+
+    public class FolderWalker
+    {
+        private readonly Func<string, string> _smallFileNameMaker;
+        private readonly DirectoryInfo _diFolderFrom;
+        private readonly DirectoryInfo _diFolderTo;
+        private readonly Regex _filePattern;
+        private readonly Regex _excludePattern;
+        private readonly Regex _smallPattern;
+
+        public FolderWalker(
+            string folderFrom, 
+            string folderTo, 
+            string filePattern, 
+            string excludePattern, 
+            string smallPattern, 
+            Func<string, string> smallFileNameMaker
+        )
+        {
+            _smallFileNameMaker = smallFileNameMaker;
+            if (folderFrom == null) throw new ArgumentNullException(nameof(folderFrom));
+            if (folderTo == null) throw new ArgumentNullException(nameof(folderTo));
+            _diFolderFrom = new DirectoryInfo(folderFrom);
+            _diFolderTo = new DirectoryInfo(folderTo);
+            if (!_diFolderFrom.Exists) throw new DirectoryNotFoundException(folderFrom);
+            if (!_diFolderTo.Exists  ) throw new DirectoryNotFoundException(folderTo  );
+            if (string.Compare(folderFrom, folderTo, StringComparison.InvariantCultureIgnoreCase) > 0) throw new InvalidOperationException("Please pick an upper bound folder alphabetically after the lower bound folder");
+            _filePattern = new Regex(filePattern);
+            _excludePattern = new Regex(excludePattern);
+            _smallPattern = new Regex(smallPattern);
+        }
+
+        public event EventHandler<FolderEventArgs> StartingFolder;
+        public event EventHandler<FolderEventArgs> EndingFolder;
+        public event EventHandler<FileEventArgs  > FoundAFile;
+
+        public void Go()
+        {
+            Go(_diFolderFrom);
+        }
+
+        private void Go(DirectoryInfo folderFrom)
+        {
+            OnStartingFolder(folderFrom);
+            foreach (var fi in 
+                folderFrom
+                    .EnumerateFiles("*",SearchOption.TopDirectoryOnly)
+                    .Where(fi => !_excludePattern.Match(fi.Name).Success)
+                    .Select(fi=>new {fileInfo=fi, fileMatch = _filePattern.Match(fi.Name).Success, smallMatch = _smallPattern.Match(fi.Name).Success })
+                    .ToArray()
+                )
+            {
+                if (fi.smallMatch) OnFoundAFile(fi.fileInfo);
+                if (!fi.fileMatch) continue;
+                var small=Path.Combine(folderFrom.FullName, _smallFileNameMaker(fi.fileInfo.Name));
+                var smallFi = new FileInfo(small);
+                if (!smallFi.Exists)
+                {
+                    OnFoundAFile(MakeSmallImage(smallFi, small));
+                }
+            }
+            OnEndingFolder(folderFrom);
+            folderFrom
+                .EnumerateDirectories()
+                .TakeWhile(di=>string.Compare(di.FullName, _diFolderTo.FullName, StringComparison.InvariantCultureIgnoreCase) > 0)
+                .ForEach(Go);
+        }
+
+        private void OnStartingFolder(DirectoryInfo di) { StartingFolder?.Invoke(this, new FolderEventArgs { DirectoryInfo = di }); }
+        private void OnEndingFolder  (DirectoryInfo di) { EndingFolder  ?.Invoke(this, new FolderEventArgs { DirectoryInfo = di }); }
+        private void OnFoundAFile    (FileInfo      fi) { FoundAFile    ?.Invoke(this, new FileEventArgs   { FileInfo      = fi }); }
+        private static FileInfo MakeSmallImage(FileInfo sourceFileInfo, string newFileName)
+        {
+            using (var img = Image.FromFile(sourceFileInfo.FullName))
+                using (var newImg = img.Scale(0.1))
+            {
+                newImg.Save(newFileName, ImageFormat.Jpeg);
+                return new FileInfo(newFileName);
+            }
+        }
+    }
+
+    public class FileEventArgs:EventArgs
+    {
+        public FileInfo FileInfo { get; set; }
+    }
+
+    public class FolderEventArgs:EventArgs
+    {
+        public DirectoryInfo DirectoryInfo { get; set; }
+    }
+
+    public static class ImageExtensions
+    {
+        public static Image Scale(this Image image, double factor)
+        {
+            var wi = (int)(image.Width * factor);
+            var he = (int)(image.Height * factor);
+            var newImage = new Bitmap(wi, he);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, wi, he);
+
+            return newImage;
+
+        }
+    }
+}
