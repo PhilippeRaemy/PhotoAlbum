ab5eb7b83ab91ef41c9be7df93b8299413d9b806
philipperaemy <philippe_raemy@swissonline.ch>
2017-03-11 21:40:54 +0100

    FormProgress implements IProgress

---
+++ AlbumWordAddin/FormProgress.cs
@@ -0,0 +1,38 @@
+﻿namespace AlbumWordAddin
+{
+    using System;
+    using System.Windows.Forms;
+
+    using UserPreferences;
+
+    public partial class FormProgress : Form, IProgress
+    {
+        public FormProgress()
+        {
+            InitializeComponent();
+        }
+
+        void buttonGo_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void InitProgress(int max)
+        {
+            progressBar.Maximum = max;
+            progressBar.Minimum = 0;
+            progressBar.Value = 0;
+            progressBar.Visible = true;
+        }
+
+        public void Progress()
+        {
+            progressBar.Value++;
+        }
+
+        public void CloseProgress()
+        {
+            progressBar.Visible = false;
+        }
+    }
+}
