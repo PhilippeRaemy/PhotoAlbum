3335d6e7f9d8c4fbd5f0969759277c98a081c9c7
philipperaemy <philippe_raemy@swissonline.ch>
2017-07-09 09:30:12 +0200

    Removed extraneous reference

--- PositionerTests/SpacerTests/SpacerTests.cs
+++ PositionerTests/SpacerTests/SpacerTests.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using AlbumWordAddin;
-    using AlbumWordAddinTests;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using MoreLinq;
     using TestHelpers;
