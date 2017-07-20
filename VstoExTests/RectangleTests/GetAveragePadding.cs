016b42a5f01f30c65c919cae683a8d90d30b6e9d
PhilippeRaemy <philippe_raemy@swissonline.ch>
2017-07-20 23:32:15 +0200

    Test comparison need no tolerance

--- VstoExTests/RectangleTests/GetAveragePadding.cs
+++ VstoExTests/RectangleTests/GetAveragePadding.cs
@@ -14,79 +14,91 @@
         [TestMethod]
         public void OneRectangleAloneHasPaddingZero()
         {
-            Assert.AreEqual(0f, new[] { new Rectangle(1, 1, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(0f, new[] { new Rectangle(1, 1, 1, 1) }.GetAveragePadding());
         }
 
         [TestMethod]
         public void TwoAdjacentRectanglesHavePaddingZero_H()
         {
-            Assert.AreEqual(0f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(2, 1, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(0f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(2, 1, 1, 1) }.GetAveragePadding());
         }
 
         [TestMethod]
         public void TwoAdjacentRectanglesHavePaddingZero_V()
         {
-            Assert.AreEqual(0f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(1, 2, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(0f, new[] {new Rectangle(1, 1, 1, 1), new Rectangle(1, 2, 1, 1)}.GetAveragePadding());
         }
 
         [TestMethod]
         public void TwoAlignedNonAdjacentRectanglesHavePaddingAsTheirDistance_H()
         {
-            Assert.AreEqual(1f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(3, 1, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(1f, new[] {new Rectangle(1, 1, 1, 1), new Rectangle(3, 1, 1, 1)}.GetAveragePadding());
         }
 
         [TestMethod]
         public void TwoAlignedNonAdjacentRectanglesHavePaddingAsTheirDistance_V()
         {
-            Assert.AreEqual(1f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(1, 3, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(1f, new[] {new Rectangle(1, 1, 1, 1), new Rectangle(1, 3, 1, 1)}.GetAveragePadding());
         }
 
         [TestMethod]
         public void ThreeAdjacentRectanglesHavePaddingZero_H()
         {
-            Assert.AreEqual(0f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(2, 1, 1, 1), new Rectangle(3, 1, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(0f,
+                new[] {new Rectangle(1, 1, 1, 1), new Rectangle(2, 1, 1, 1), new Rectangle(3, 1, 1, 1)}
+                    .GetAveragePadding());
         }
 
         [TestMethod]
         public void ThreeAdjacentRectanglesHavePaddingZero_V()
         {
-            Assert.AreEqual(0f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(1, 2, 1, 1), new Rectangle(1, 3, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(0f,
+                new[] {new Rectangle(1, 1, 1, 1), new Rectangle(1, 2, 1, 1), new Rectangle(1, 3, 1, 1)}
+                    .GetAveragePadding());
         }
 
         [TestMethod]
         public void ThreeAlignedNonAdjacentRectanglesHavePaddingAsTheirDistance_H()
         {
-            Assert.AreEqual(1f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(3, 1, 1, 1), new Rectangle(5, 1, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(1f,
+                new[] {new Rectangle(1, 1, 1, 1), new Rectangle(3, 1, 1, 1), new Rectangle(5, 1, 1, 1)}
+                    .GetAveragePadding());
         }
 
         [TestMethod]
         public void ThreeAlignedNonAdjacentRectanglesHavePaddingAsTheirDistance_V()
         {
-            Assert.AreEqual(1f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(1, 3, 1, 1), new Rectangle(1, 5, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(1f,
+                new[] {new Rectangle(1, 1, 1, 1), new Rectangle(1, 3, 1, 1), new Rectangle(1, 5, 1, 1)}
+                    .GetAveragePadding());
         }
 
         [TestMethod]
         public void ThreeAlignedNonAdjacentRectanglesHavePaddingAsTheirAverageDistance_H()
         {
-            Assert.AreEqual(1.5f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(3, 1, 1, 1), new Rectangle(6, 1, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(1.5f,
+                new[] {new Rectangle(1, 1, 1, 1), new Rectangle(3, 1, 1, 1), new Rectangle(6, 1, 1, 1)}
+                    .GetAveragePadding());
         }
 
         [TestMethod]
         public void ThreeAlignedNonAdjacentRectanglesHavePaddingAsTheirAverageDistance_V()
         {
-            Assert.AreEqual(1.5f, new[] { new Rectangle(1, 1, 1, 1), new Rectangle(1, 3, 1, 1), new Rectangle(6, 5, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(1.5f,
+                new[] {new Rectangle(1, 1, 1, 1), new Rectangle(1, 3, 1, 1), new Rectangle(6, 5, 1, 1)}
+                    .GetAveragePadding());
         }
 
         [TestMethod]
         public void TwoOverlappingdRectanglesHaveNegativePaddingAsTheirDistance_H()
         {
-            Assert.AreEqual(-1f, new[] { new Rectangle(1, 1, 2, 1), new Rectangle(2, 1, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(-1f, new[] {new Rectangle(1, 1, 2, 1), new Rectangle(2, 1, 1, 1)}.GetAveragePadding());
         }
 
         [TestMethod]
         public void TwoOverlappingdRectanglesHaveNegativePaddingAsTheirDistance_V()
         {
-            Assert.AreEqual(-1f, new[] { new Rectangle(1, 1, 1, 2), new Rectangle(1, 2, 1, 1) }.GetAveragePadding(), 0f);
+            Assert.AreEqual(-1f, new[] {new Rectangle(1, 1, 1, 2), new Rectangle(1, 2, 1, 1)}.GetAveragePadding());
         }
 
         [TestMethod]
@@ -98,7 +110,7 @@
                 new Rectangle(3, 1, 1, 1),
                 new Rectangle(1, 3, 1, 1),
                 new Rectangle(3, 3, 1, 1)
-            }.GetAveragePadding(), 0f);
+            }.GetAveragePadding());
         }
 
         [TestMethod]
@@ -110,7 +122,7 @@
                 new Rectangle(3, 1.5f, 1, 1),
                 new Rectangle(1, 3, 1, 1),
                 new Rectangle(3, 3.5f, 1, 1)
-            }.GetAveragePadding(), 0f);
+            }.GetAveragePadding());
         }
 
         [TestMethod]
@@ -122,7 +134,7 @@
                 new Rectangle(5, 1, 1, 1),
                 new Rectangle(1, 1, 1, 1),
                 new Rectangle(7, 1, 1, 1)
-            }.GetAveragePadding(), 0f);
+            }.GetAveragePadding());
         }
 
         [TestMethod]
@@ -134,7 +146,7 @@
                 new Rectangle(3, 3, 1, 1),
                 new Rectangle(1, 5, 1, 1),
                 new Rectangle(3, 7, 1, 1)
-            }.GetAveragePadding(), 0f);
+            }.GetAveragePadding());
         }
     }
 }
