96c30680d8ba816c40b388152f9f12a5005f6528
philipperaemy <philippe_raemy@swissonline.ch>
2017-07-25 08:04:51 +0200

    EnumerableExtensions.AffordableToArray

---
+++ VstoEx/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace VstoEx.Extensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EnumerableExtensions
+    {
+        public static T[] AffordableToArray<T>(this IEnumerable<T> enumerable)
+            => enumerable as T[] ?? enumerable.ToArray();
+    }
+}
