fa7981f1cdb5b94b24e6b69c0048220790d5d130
philipperaemy <philippe_raemy@swissonline.ch>
2017-07-30 07:31:03 +0200

    FitIn works with absolute spacing

--- VstoEx/Geometry/Rectangle.cs
+++ VstoEx/Geometry/Rectangle.cs
@@ -77,10 +77,10 @@ namespace VstoEx.Geometry
         public Rectangle ScaleInPlace(float scale)
             => new Rectangle(Center, scale * Width, scale * Height);
 
-        public Rectangle FitIn(Rectangle other, float fitLeftPerc, float fitTopPerc, float spacing) {
-            if (Math.Abs(spacing) > Epsilon)
+        public Rectangle FitIn(Rectangle other, float fitLeftPerc, float fitTopPerc, float absoluteSpacing) {
+            if (Math.Abs(absoluteSpacing) > Epsilon)
             {
-                other=new Rectangle(other.Left + spacing, other.Top + spacing, other.Width - 2 * spacing, other.Height - 2 * spacing);   
+                other=new Rectangle(other.Left + absoluteSpacing, other.Top + absoluteSpacing, other.Width - 2 * absoluteSpacing, other.Height - 2 * absoluteSpacing);   
             }
             var scale = new[] { other.Width / Width, other.Height / Height }.Min();
             var newWidth  = Width * scale;
