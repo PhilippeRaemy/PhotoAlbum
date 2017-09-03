6251ff7644e8bd3071493a7899c603053d3b9a65
philipperaemy <philippe_raemy@swissonline.ch>
2017-09-03 18:28:00 +0200

    DualFile repairs broken picture links, if the picture file is found in the document folder, or a nearby folder (descendant, parent or siblings)

--- AlbumWordAddin/DualFile.cs
+++ AlbumWordAddin/DualFile.cs
@@ -2,9 +2,11 @@ namespace AlbumWordAddin
 {
     using System;
     using System.IO;
+    using System.Linq;
 
     internal class DualFile
     {
+        readonly string _documentFullFileName;
         public FileInfo FileInfo { get; }
         public FileInfo DualFileInfo { get; }
         public bool DualExists => DualFileInfo.Exists;
@@ -12,13 +14,24 @@ namespace AlbumWordAddin
 
         public DualFile(string fullFileName, string documentFullFileName, Func<string, string> fileNameMaker)
         {
-            FileInfo = new FileInfo(fullFileName);
+            _documentFullFileName = documentFullFileName;
+            FileInfo =  ValidateFileInfo(new FileInfo(fullFileName));
 
             if (FileInfo.DirectoryName == null) return;
-            DualFileInfo = new FileInfo(Path.Combine(FileInfo.DirectoryName, fileNameMaker(FileInfo.Name)));
+            DualFileInfo = ValidateFileInfo(new FileInfo(Path.Combine(FileInfo.DirectoryName, fileNameMaker(FileInfo.Name))));
         }
 
-
-
+        FileInfo ValidateFileInfo(FileInfo fi)
+        {
+            if(fi.Exists) return fi;
+            var documentFileInfo = new FileInfo(_documentFullFileName);
+            if (documentFileInfo.DirectoryName == null) return fi;
+            var documentFolder = new DirectoryInfo(documentFileInfo.DirectoryName);
+            var candidate = documentFolder.GetFiles(fi.Name).FirstOrDefault();
+            if (candidate?.Exists ?? false) return candidate;
+            candidate = documentFolder.Parent?.GetFiles(fi.Name).FirstOrDefault();
+            if (candidate?.Exists ?? false) return candidate;
+            return fi;
+        }
     }
 }
