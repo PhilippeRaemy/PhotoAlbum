18f43b2cd7e50dd7cb771d2901ddbd08b4fb307b
philipperaemy <philippe_raemy@swissonline.ch>
2019-11-03 10:43:20 +0100

    Create class NewPositioner:IPositioner and duplicate unit tests

---
+++ AlbumWordAddin/Positioning/NewPositioner.cs
@@ -0,0 +1,14 @@
+﻿namespace AlbumWordAddin.Positioning
+{
+    using System;
+    using System.Collections.Generic;
+    using VstoEx.Geometry;
+
+    public class NewPositioner : IPositioner
+    {
+        public IEnumerable<Rectangle> DoPosition(PositionerParms parms, Rectangle clientArea, IEnumerable<Rectangle> rectangles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
