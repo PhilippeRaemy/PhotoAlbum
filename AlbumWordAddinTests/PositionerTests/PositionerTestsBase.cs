a22068ac6e3c49763933516098de75a95d67b458
philipperaemy <philippe_raemy@swissonline.ch>
2019-11-30 21:00:34 +0100

    Separate positioner run from rectangles series comparison

--- AlbumWordAddinTests/PositionerTests/PositionerTestsBase.cs
+++ AlbumWordAddinTests/PositionerTests/PositionerTestsBase.cs
@@ -13,16 +13,19 @@ namespace AlbumWordAddinTests.PositionerTests
     {
         protected abstract IPositioner GetNewPositioner();
 
-        internal void Run(Rectangle clientArea, IEnumerable<Rectangle> rectangles, PositionerParms pos, IEnumerable<Rectangle> expected, string label)
+        internal void Run(Rectangle clientArea, IEnumerable<Rectangle> rectangles, PositionerParms pos, IEnumerable<Rectangle> expected, string label) 
+            => Test(GetNewPositioner().DoPosition(pos, clientArea, rectangles), expected, label);
+
+        internal void Test(IEnumerable<Rectangle> rectangles, IEnumerable<Rectangle> expected, string label)
         {
-            var rc = GetNewPositioner().DoPosition(pos, clientArea, rectangles).CheapToArray();
             expected = expected.CheapToArray();
+            var rc = rectangles.CheapToArray();
             Assert.AreEqual(expected.Count(), rc.Length, $"{label}: Results length");
-            var results = expected.EquiZip(rc, (e, r) => new {expected = e, results = r})
-                .Select((r, i) => new {i, r.expected, r.results, success = r.expected.Equals(r.results)})
+            var results = expected.EquiZip(rc, (e, r) => new { expected = e, results = r })
+                .Select((r, i) => new { i, r.expected, r.results, success = r.expected.Equals(r.results) })
                 .ToArray();
 
-            Assert.IsTrue(results.All(r=>r.success), results.Select(r=> $"{Environment.NewLine}{r.expected} {(r.success ? "==" : "<>")} {r.results}").ToDelimitedString(","), $"{label}: equality");
+            Assert.IsTrue(results.All(r => r.success), results.Select(r => $"{Environment.NewLine}{r.expected} {(r.success ? "==" : "<>")} {r.results}").ToDelimitedString(","), $"{label}: equality");
         }
     }
 }
