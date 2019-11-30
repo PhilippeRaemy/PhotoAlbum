b367c535b8358ed34da848a628665d6a0c68f6f5
philipperaemy <philippe_raemy@swissonline.ch>
2019-11-30 21:44:01 +0100

    Use expression body

--- AlbumWordAddinTests/PositionerTests/NewPositionerTests.cs
+++ AlbumWordAddinTests/PositionerTests/NewPositionerTests.cs
@@ -33,22 +33,13 @@
         static readonly PositionerParms VFlatTopPos  = new PositionerParms { Rows = 4, Cols = 1, HShape = HShape.Flat, VShape = VShape.Top , Margin = 0, Spacing = 0    };
 
         [TestMethod]
-        public void TestPositioner_1x1()
-        {
-            Run(R1X1, new[] { R1X1 }, TrivialPos, new[] { R1X1 }, string.Empty);
-        }
+        public void TestPositioner_1x1() => Run(R1X1, new[] { R1X1 }, TrivialPos, new[] { R1X1 }, string.Empty);
 
         [TestMethod]
-        public void TestPositioner_VFlat2X1()
-        {
-            Run(R1X1, new[] {R1X1, R1X1}, VFlat2X1Pos, new[] {new Rectangle(.25f, 0, .5f, .5f), new Rectangle(.25f, .5f, .5f, .5f)}, string.Empty);
-        }
+        public void TestPositioner_VFlat2X1() => Run(R1X1, new[] {R1X1, R1X1}, VFlat2X1Pos, new[] {new Rectangle(.25f, 0, .5f, .5f), new Rectangle(.25f, .5f, .5f, .5f)}, string.Empty);
 
         [TestMethod]
-        public void TestPositioner_HFlat1X2()
-        {
-            new[] { .5f, 1f, 1.5f, 2f }.ForEach(TestPositioner_HFlat1X2);
-        }
+        public void TestPositioner_HFlat1X2() => new[] { .5f, 1f, 1.5f, 2f }.ForEach(TestPositioner_HFlat1X2);
 
         static void TestPositioner_HFlat1X2(float factor)
         {
@@ -61,10 +52,7 @@
         }
 
         [TestMethod]
-        public void TestPositioner_HFlat1X3()
-        {
-            new[] { .5f, 1f, 1.5f, 2f }.ForEach(TestPositioner_HFlat1X3);
-        }
+        public void TestPositioner_HFlat1X3() => new[] { .5f, 1f, 1.5f, 2f }.ForEach(TestPositioner_HFlat1X3);
 
         static void TestPositioner_HFlat1X3(float factor)
         {
@@ -79,123 +67,63 @@
         }
 
         [TestMethod]
-        public void TestPositioner_FourInaRowFlatNoSpacing1()
-        {
-            Run(R4X1, R1X1.Range(4), HFlatPos, R1X1.Range(4, 1, 0), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaRowFlatNoSpacing1() => Run(R4X1, R1X1.Range(4), HFlatPos, R1X1.Range(4, 1, 0), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowFlatNoSpacing2()
-        {
-            Run(R4X1, R4X2.Range(4), HFlatPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 1, 0), nameof(R4X2));
-        }
+        public void TestPositioner_FourInaRowFlatNoSpacing2() => Run(R4X1, R4X2.Range(4), HFlatPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 1, 0), nameof(R4X2));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowFlatNoSpacing3()
-        {
-            Run(R4X1, R2X4.Range(4), HFlatPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 1, 0), nameof(R2X4));
-        }
+        public void TestPositioner_FourInaRowFlatNoSpacing3() => Run(R4X1, R2X4.Range(4), HFlatPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 1, 0), nameof(R2X4));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowFlatSpacing()
-        {
-            Run(R4X1, R1X1.Range(4), HFlatPosPad, R1X1.MoveBy(HFlatPosPad.Spacing, HFlatPosPad.Spacing).Grow(1 - 2 * HFlatPosPad.Spacing).Range(4, 1, 0), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaRowFlatSpacing() => Run(R4X1, R1X1.Range(4), HFlatPosPad, R1X1.MoveBy(HFlatPosPad.Spacing, HFlatPosPad.Spacing).Grow(1 - 2 * HFlatPosPad.Spacing).Range(4, 1, 0), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowLeftNoSpacing1()
-        {
-            Run(R4X1, R1X1.Range(4), HFlatLeftPos, R1X1.Range(4, 1, 0), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaRowLeftNoSpacing1() => Run(R4X1, R1X1.Range(4), HFlatLeftPos, R1X1.Range(4, 1, 0), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowLeftNoSpacing2()
-        {
-            Run(R4X1, R4X2.Range(4), HFlatLeftPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 1, 0), nameof(R4X2));
-        }
+        public void TestPositioner_FourInaRowLeftNoSpacing2() => Run(R4X1, R4X2.Range(4), HFlatLeftPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 1, 0), nameof(R4X2));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowLeftNoSpacing3()
-        {
-            Run(R4X1, R2X4.Range(4), HFlatLeftPos, R2X4.Grow(.25f).Range(4, 1, 0), nameof(R2X4));
-        }
+        public void TestPositioner_FourInaRowLeftNoSpacing3() => Run(R4X1, R2X4.Range(4), HFlatLeftPos, R2X4.Grow(.25f).Range(4, 1, 0), nameof(R2X4));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowTopNoSpacing1()
-        {
-            Run(R4X1, R1X1.Range(4), HFlatTopPos, R1X1.Range(4, 1, 0), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaRowTopNoSpacing1() => Run(R4X1, R1X1.Range(4), HFlatTopPos, R1X1.Range(4, 1, 0), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowTopNoSpacing2()
-        {
-            Run(R4X1, R4X2.Range(4), HFlatTopPos, R4X2.Grow(.25f).Range(4, 1, 0), nameof(R4X2));
-        }
+        public void TestPositioner_FourInaRowTopNoSpacing2() => Run(R4X1, R4X2.Range(4), HFlatTopPos, R4X2.Grow(.25f).Range(4, 1, 0), nameof(R4X2));
 
         [TestMethod]
-        public void TestPositioner_FourInaRowTopNoSpacing3()
-        {
-            Run(R4X1, R2X4.Range(4), HFlatTopPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 1, 0), nameof(R2X4));
-        }
+        public void TestPositioner_FourInaRowTopNoSpacing3() => Run(R4X1, R2X4.Range(4), HFlatTopPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 1, 0), nameof(R2X4));
 
         [TestMethod]
-        public void TestPositioner_FourInaColFlatNoSpacing1()
-        {
-            Run(R1X4, R1X1.Range(4), VFlatPos, R1X1.Range(4, 0, 1), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaColFlatNoSpacing1() => Run(R1X4, R1X1.Range(4), VFlatPos, R1X1.Range(4, 0, 1), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaColFlatNoSpacing2()
-        {
-            Run(R1X4, R4X2.Range(4), VFlatPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 0, 1), nameof(R4X2));
-        }
+        public void TestPositioner_FourInaColFlatNoSpacing2() => Run(R1X4, R4X2.Range(4), VFlatPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 0, 1), nameof(R4X2));
 
         [TestMethod]
-        public void TestPositioner_FourInaColFlatNoSpacing3()
-        {
-            Run(R1X4, R2X4.Range(4), VFlatPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 0, 1), nameof(R2X4));
-        }
+        public void TestPositioner_FourInaColFlatNoSpacing3() => Run(R1X4, R2X4.Range(4), VFlatPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 0, 1), nameof(R2X4));
 
         [TestMethod]
-        public void TestPositioner_FourInaColFlatSpacing()
-        {
-            Run(R1X4, R1X1.Range(4), VFlatPosPad, R1X1.MoveBy(VFlatPosPad.Spacing, VFlatPosPad.Spacing).Grow(1 - 2 * VFlatPosPad.Spacing).Range(4, 0, 1), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaColFlatSpacing() => Run(R1X4, R1X1.Range(4), VFlatPosPad, R1X1.MoveBy(VFlatPosPad.Spacing, VFlatPosPad.Spacing).Grow(1 - 2 * VFlatPosPad.Spacing).Range(4, 0, 1), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaColLeftNoSpacing1()
-        {
-            Run(R1X4, R1X1.Range(4), VFlatLeftPos, R1X1.Range(4, 0, 1), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaColLeftNoSpacing1() => Run(R1X4, R1X1.Range(4), VFlatLeftPos, R1X1.Range(4, 0, 1), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaColLeftNoSpacing2()
-        {
-            Run(R1X4, R4X2.Range(4), VFlatLeftPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 0, 1), nameof(R4X2));
-        }
+        public void TestPositioner_FourInaColLeftNoSpacing2() => Run(R1X4, R4X2.Range(4), VFlatLeftPos, R4X2.MoveBy(0, .25f).Grow(.25f).Range(4, 0, 1), nameof(R4X2));
 
         [TestMethod]
-        public void TestPositioner_FourInaColLeftNoSpacing3()
-        {
-            Run(R1X4, R2X4.Range(4), VFlatLeftPos, R2X4.Grow(.25f).Range(4, 0, 1), nameof(R2X4));
-        }
+        public void TestPositioner_FourInaColLeftNoSpacing3() => Run(R1X4, R2X4.Range(4), VFlatLeftPos, R2X4.Grow(.25f).Range(4, 0, 1), nameof(R2X4));
 
         [TestMethod]
-        public void TestPositioner_FourInaColTopNoSpacing1()
-        {
-            Run(R1X4, R1X1.Range(4), VFlatTopPos, R1X1.Range(4, 0, 1), nameof(R1X1));
-        }
+        public void TestPositioner_FourInaColTopNoSpacing1() => Run(R1X4, R1X1.Range(4), VFlatTopPos, R1X1.Range(4, 0, 1), nameof(R1X1));
 
         [TestMethod]
-        public void TestPositioner_FourInaColTopNoSpacing2()
-        {
-            Run(R1X4, R4X2.Range(4), VFlatTopPos, R4X2.Grow(.25f).Range(4, 0, 1), nameof(R4X2));
-        }
+        public void TestPositioner_FourInaColTopNoSpacing2() => Run(R1X4, R4X2.Range(4), VFlatTopPos, R4X2.Grow(.25f).Range(4, 0, 1), nameof(R4X2));
 
         [TestMethod]
-        public void TestPositioner_FourInaColTopNoSpacing3()
-        {
-            Run(R1X4, R2X4.Range(4), VFlatTopPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 0, 1), nameof(R2X4));
-        }
+        public void TestPositioner_FourInaColTopNoSpacing3() => Run(R1X4, R2X4.Range(4), VFlatTopPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 0, 1), nameof(R2X4));
     }
 }
