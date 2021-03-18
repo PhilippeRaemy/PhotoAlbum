8d1a3dd6642e4fa1b784ad15bc5120c86e951baa
philipperaemy <philippe_raemy@swissonline.ch>
2021-03-18 22:21:38 +0100

    Added tests for margin with spacing (failed so far)

--- TestsAlbumWordAddin/PositionerTests/GridPositionerTests.cs
+++ TestsAlbumWordAddin/PositionerTests/GridPositionerTests.cs
@@ -8,6 +8,8 @@ using VstoEx.Geometry;
 
 namespace TestsAlbumWordAddin.PositionerTests
 {
+    using System.Security.Cryptography.X509Certificates;
+
     [TestClass]
     public class GridPositionerTests : PositionerTestsBase
     {
@@ -16,7 +18,7 @@ namespace TestsAlbumWordAddin.PositionerTests
         internal static readonly Rectangle R1X1 = new Rectangle(0, 0, 1, 1);
         static readonly Rectangle R4X1 = new Rectangle(0, 0, 4, 1);
         static readonly Rectangle R1X4 = new Rectangle(0, 0, 1, 4);
-        static readonly Rectangle R4X4 = new Rectangle(0, 0, 4, 4);
+        static readonly Rectangle R2X2 = new Rectangle(0, 0, 2, 2);
         static readonly Rectangle R4X2 = new Rectangle(0, 0, 4, 2);
         static readonly Rectangle R2X4 = new Rectangle(0, 0, 2, 4);
         static readonly PositionerParms TrivialPos   = new PositionerParms { Rows = 1, Cols = 1, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Spacing = 0    };
@@ -30,7 +32,9 @@ namespace TestsAlbumWordAddin.PositionerTests
         static readonly PositionerParms VFlatPos     = new PositionerParms { Rows = 4, Cols = 1, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Spacing = 0    };
         static readonly PositionerParms VFlatPosPad  = new PositionerParms { Rows = 4, Cols = 1, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Spacing = 0.1f };
         static readonly PositionerParms VFlatLeftPos = new PositionerParms { Rows = 4, Cols = 1, HShape = HShape.Left, VShape = VShape.Flat, Margin = 0, Spacing = 0    };
-        static readonly PositionerParms VFlatTopPos  = new PositionerParms { Rows = 4, Cols = 1, HShape = HShape.Flat, VShape = VShape.Top , Margin = 0, Spacing = 0    };
+        static readonly PositionerParms VFlatTopPos  = new PositionerParms { Rows = 4, Cols = 1, HShape = HShape.Flat, VShape = VShape.Top, Margin = 0, Spacing = 0 };
+        static readonly PositionerParms VFlatTopPosMargin = new PositionerParms { Rows = 4, Cols = 1, HShape = HShape.Flat, VShape = VShape.Top, Margin = 0.1f, Spacing = 0 };
+
 
         [TestMethod]
         public void TestPositioner_1x1()
@@ -136,6 +140,85 @@ namespace TestsAlbumWordAddin.PositionerTests
             Run(R1X4, R1X1.Range(4), VFlatTopPos, R1X1.Range(4, 0, 1), nameof(R1X1));
             Run(R1X4, R4X2.Range(4), VFlatTopPos, R4X2.Grow(.25f).Range(4, 0, 1), nameof(R4X2));
             Run(R1X4, R2X4.Range(4), VFlatTopPos, R2X4.MoveBy(.25f, 0).Grow(.25f).Range(4, 0, 1), nameof(R2X4));
+        }
+
+        [TestMethod]
+        public void TestPositioner_FourInaColTopMarginNoSpacing()
+        {
+            Run(R1X4, R1X1.Range(4), VFlatTopPosMargin, R1X1.Range(4, 0, 1).Select((r, i) => r.Grow(.8f).MoveBy(.1f,.1f - i*.05f)), nameof(R1X1));
+        }
+
+        [TestMethod]
+        public void TestPositioner_FourInSquareNoMarginNoSpacing()
+        {
+            Run(R2X2, R1X1.Range(4),
+                new PositionerParms { Rows = 2, Cols = 2, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0, Spacing = 0 },
+                new[]
+                {
+                    R1X1, 
+                    R1X1.MoveBy(1, 0), 
+                    R1X1.MoveBy(0, 1), 
+                    R1X1.MoveBy(1, 1)
+                },
+                "TestPositioner_FourInSquareNoMarginNoSpacing"
+            );
+        }
+
+        [TestMethod]
+        public void TestPositioner_FourInSquareSmallMarginNoSpacing()
+        {
+            Run(R2X2, R1X1.Range(4),
+                new PositionerParms { Rows = 2, Cols = 2, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0.1f, Spacing = 0 },
+                new[] {
+                    R1X1.Grow(.9f).MoveBy(.1f, .1f),
+                    R1X1.Grow(.9f).MoveBy(1, .1f),
+                    R1X1.Grow(.9f).MoveBy(.1f, 1),
+                    R1X1.Grow(.9f).MoveBy(1, 1)
+                },
+                "TestPositioner_FourInSquareNoMarginNoSpacing"
+            );
+        }
+        [TestMethod]
+        public void TestPositioner_FourInSquareLargeMarginNoSpacing()
+        {
+            Run(R2X2, R1X1.Range(4),
+                new PositionerParms { Rows = 2, Cols = 2, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0.5f, Spacing = 0 },
+                new[] {
+                    R1X1.Grow(.5f).MoveBy(.5f, .5f),
+                    R1X1.Grow(.5f).MoveBy(1, .5f),
+                    R1X1.Grow(.5f).MoveBy(.5f, 1),
+                    R1X1.Grow(.5f).MoveBy(1, 1)
+                },
+                "TestPositioner_FourInSquareNoMarginNoSpacing"
+            );
+        }
+        [TestMethod]
+        public void TestPositioner_FourInSquareSmallMarginSmallSpacing()
+        {
+            Run(R2X2, R1X1.Range(4),
+                new PositionerParms { Rows = 2, Cols = 2, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0.1f, Spacing = .2f },
+                new[] {
+                    R1X1.Grow(.8f).MoveBy(.1f, .1f),
+                    R1X1.Grow(.8f).MoveBy(1.1f, .1f),
+                    R1X1.Grow(.8f).MoveBy(.1f, 1.1f),
+                    R1X1.Grow(.8f).MoveBy(1.1f, 1.1f)
+                },
+                "TestPositioner_FourInSquareNoMarginNoSpacing"
+            );
+        }
+        [TestMethod]
+        public void TestPositioner_FourInSquareLargeMarginLargeSpacing()
+        {
+            Run(R2X2, R1X1.Range(4),
+                new PositionerParms { Rows = 2, Cols = 2, HShape = HShape.Flat, VShape = VShape.Flat, Margin = 0.5f, Spacing = 0.5f },
+                new[] {
+                    R1X1.Grow(.25f).MoveBy(.5f, .5f),
+                    R1X1.Grow(.25f).MoveBy(1.25f, .5f),
+                    R1X1.Grow(.25f).MoveBy(.5f, 1.25f),
+                    R1X1.Grow(.25f).MoveBy(1.25f, 1.25f)
+                },
+                "TestPositioner_FourInSquareNoMarginNoSpacing"
+            );
         }
     }
 }
