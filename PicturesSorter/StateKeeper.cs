26252ee79efc427f288a0b066a01b435ed2f7a30
Philippe <Philippe@SERVER02>
2022-06-24 09:04:17 +0200

    Add StateKeeper class

---
+++ PicturesSorter/StateKeeper.cs
@@ -0,0 +1,32 @@
+﻿namespace PicturesSorter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    class StateKeeper:IDisposable
+    {
+        readonly Stack<Action> _unwindActions = new Stack<Action>();
+
+        public StateKeeper Hourglass(Form form)
+        {
+            var previousCursor = form.Cursor;
+            _unwindActions.Push(() => form.Cursor = previousCursor);
+            form.Cursor = Cursors.WaitCursor;
+            return this;
+        }
+
+        public StateKeeper Disable(Control control)
+        {
+            _unwindActions.Push(() => control.Enabled=true);
+            control.Enabled = false;
+            return this;
+        }
+
+        public void Dispose()
+        {
+            while(_unwindActions.Any()) _unwindActions.Pop().Invoke();
+        }
+    }
+}
