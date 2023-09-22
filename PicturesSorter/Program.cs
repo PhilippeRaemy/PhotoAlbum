d51da69819d0817149b434ae134bdc62f75a1efe
Philippe <Philippe@SERVER02>
2023-09-22 23:58:18 +0200

    Take SimilarPicturesHandler out of SimilarPicturesForm

--- PicturesSorter/Program.cs
+++ PicturesSorter/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace PicturesSorter
 {
     using System;
+    using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Windows.Forms;
 
@@ -10,12 +12,24 @@
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-       {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.ThreadException += Application_ThreadException;
-            Application.Run(new PictureSorterForm());
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.ThreadException += Application_ThreadException;
+                Application.Run(new PictureSorterForm());
+            }
+
+            if (args.Any(a => string.Equals(a, "--deduplicate", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                var folderName =
+                    args.SkipWhile(a => !string.Equals(a, "--folder", StringComparison.InvariantCultureIgnoreCase))
+                        .Skip(1).FirstOrDefault() ?? throw new InvalidOperationException("Missing folder name");
+                var folder = new DirectoryInfo(folderName);
+                if (!folder.Exists) throw new DirectoryNotFoundException($"Folder {folder.FullName} not found!");
+            }
         }
 
         static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
