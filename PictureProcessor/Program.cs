4a2ab48880f57b0f93a98b39bf38d27ce0a7c649
philipperaemy <philippe_raemy@swissonline.ch>
2023-10-23 16:38:24 +0200

    Draft CLI in PictureProcessor

--- PictureProcessor/Program.cs
+++ PictureProcessor/Program.cs
@@ -6,10 +6,14 @@
 
     internal class Program
     {
-        static string _rootPath;
+        static string _rootPath = ".";
         static bool _recurse;
-        static string _command;
+        static bool _deduplicate;
+        static bool _dryrun;
+        static bool _noRecycle;
         static bool _verbose;
+        static bool _gui;
+        static int _similarity=99;
 
         static int Main(string[] args)
         {
@@ -17,22 +21,33 @@
                 .AddHelpSwitch()
                 .WithErrorWriter(Console.Error.WriteLine)
                 .WithHelpWriter(Console.WriteLine)
-                .AddStringParameter("Command", a => _command = a, "The command to execute")
                 .AddOptionalStringParameter("RootPath", a => _rootPath = a, "The path from which to explore pictures", ".")
                 .AddSwitch("Recurse", () => _recurse = true, "Explore subfolders")
-                .AddOptionalIntegerParameter("Siblings", a => int.Parse(a, NumberStyles.Integer, CultureInfo.InvariantCulture),
-                    "Explore sibling folders up to this level", "0")
+                .AddSwitch("DryRun", () => _dryrun = true, "Only display work at hand")
+                .AddSwitch("Deduplicate", () => _deduplicate = true, "Deduplicate pictures")
+                .AddSwitch("NoRecycleBin", () => _noRecycle = true, "Deduplicate pictures")
+                .AddSwitch("Verbose", () => _verbose = true, "Produce verbose console output")
+                .AddSwitch("GUI", () => _gui= true, "Show graphical use interface")
+                .AddOptionalIntegerParameter("Similarity", a => _similarity = int.Parse(a, NumberStyles.Integer, CultureInfo.InvariantCulture),
+                    "similarity factor for deduplicate", "99")
                 .AddSwitch("Verbose", () => _verbose = true, "Produce verbose output")
                 .Run(args);
             if(_verbose) parser.EchoParameters();
-            switch (_command.ToLowerInvariant())
-            {
-                case "map": break;
-                default: 
-                    Console.Error.WriteLine("Fatal error: unsupported command `{_command}`.");
-                    return -1; 
-            }
+            if (_gui)
+                ShowGui(_rootPath, _recurse, _similarity);
+            else if (_deduplicate)
+                DeduplicatePictures(_rootPath, _recurse, _noRecycle, _dryrun, _verbose, _similarity);
             return 0;
+        }
+
+        static void ShowGui(string rootPath, bool recurse, int similarity)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void DeduplicatePictures(string rootPath, bool recurse, bool noRecycle, bool dryrun, bool verbose, int similarity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
