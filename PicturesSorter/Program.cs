6cc82494acf13b2f07a680db242cd85c2c55933d
Philippe <Philippe@SERVER02>
2025-04-19 21:41:25 +0200

    remove attempt to CLI on that exe

--- PicturesSorter/Program.cs
+++ PicturesSorter/Program.cs
@@ -18,29 +18,10 @@
         [STAThread]
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.ThreadException += Application_ThreadException;
-                Application.Run(new PictureSorterForm());
-            }
-
-            if (args.Any(a => string.Equals(a, "--deduplicate", StringComparison.InvariantCultureIgnoreCase)))
-            {
-                var folderName =
-                    args.SkipWhile(a => !string.Equals(a, "--deduplicate", StringComparison.InvariantCultureIgnoreCase))
-                        .Skip(1).FirstOrDefault() ?? throw new InvalidOperationException("Missing folder name");
-                var folder = new DirectoryInfo(folderName);
-                if (!folder.Exists) throw new DirectoryNotFoundException($"Folder {folder.FullName} not found!");
-                var similarPictureForm = new SimilarPicturesForm();
-
-                Task<Dictionary<PictureSignature, List<PictureSignature>>> loadPictures = similarPictureForm.LoadPictures(folder, true);
-                Task.WaitAll(loadPictures);
-                var pics = loadPictures.Result;
-                Debugger.Break();
-                Console.WriteLine(pics);
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
+            Application.Run(new PictureSorterForm());
         }
 
         static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
